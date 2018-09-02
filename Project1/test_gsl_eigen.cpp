eca010097b278e80c260f790f1d4ab49f0f2d85d
RafaelS <rafaelcpii93@gmail.com>
2018-09-02 15:17:37 -0300

    Test calculate inverse matrix and determinant.

--- Project1/test_gsl_eigen.cpp
+++ Project1/test_gsl_eigen.cpp
@@ -38,10 +38,18 @@ main (void)
     std::complex<double> *test;
     test = new std::complex<double>[2];
 
-    // complex double *evals;
-    // evals = (complex double*)malloc( sizeof(complex double));
-    // double complex evals[4];
-    // double complex evecs[9]
+    std::complex<double> *evals = new std::complex<double>[4];
+    std::complex<double>**evecs = new std::complex<double>*[4];
+
+    for(int i = 0; i < 4; i++){
+        evecs[i] = new std::complex<double>[4];
+    }
+
+
+    gsl_permutation *p=gsl_permutation_alloc(4);
+    gsl_matrix_complex *S=gsl_matrix_complex_alloc(4,4);
+    gsl_matrix_complex *Sinv=gsl_matrix_complex_alloc(4,4);
+
   {
     int i, j;
 
@@ -54,22 +62,54 @@ main (void)
 
         printf ("eigenvalue = %g + %gi\n",
                 GSL_REAL(eval_i), GSL_IMAG(eval_i));
-        std::real(test[0]) = GSL_REAL(eval_i); std::imag(test[0]) = GSL_IMAG(eval_i);
-        std::cout << test[0] << std::endl;
+        std::real(evals[i]) = GSL_REAL(eval_i); std::imag(evals[i]) = GSL_IMAG(eval_i);
+        std::cout << evals[i] << std::endl;
         printf ("eigenvector = \n");
         for (j = 0; j < 4; ++j)
           {
             gsl_complex z =
               gsl_vector_complex_get(&evec_i.vector, j);
             printf("%g + %gi\n", GSL_REAL(z), GSL_IMAG(z));
-            std::real(test[1]) = GSL_REAL(z); std::imag(test[1]) = GSL_IMAG(z);
-            std::cout << test[1] << std::endl;
+            std::real(evecs[i][j]) = GSL_REAL(z); std::imag(evecs[i][j]) = GSL_IMAG(z);
+            gsl_matrix_complex_set(S,i,j,z);
+            std::cout << evecs[i][j] << std::endl;
           }
       }
   }
 
   gsl_vector_complex_free(eval);
   gsl_matrix_complex_free(evec);
+
+    int i, j, s;
+  
+
+    // gsl_matrix_complex_set(S,0,1,gsl_complex_rect(1.0,-1.0));
+    // gsl_matrix_complex_set(S,1,0,gsl_complex_rect(1.5,0.8));
+    // gsl_matrix_complex_set(S,1,1,gsl_complex_rect(-0.5,1.4));
+
+    gsl_linalg_complex_LU_decomp(S,p,&s);
+    gsl_linalg_complex_LU_invert(S,p,Sinv);
+
+    gsl_complex det = gsl_linalg_complex_LU_det(S,s);
+
+    std::complex<double> deter;
+
+    std::real(deter) = GSL_REAL(det); std::imag(deter) = GSL_IMAG(det); 
+
+    std::cout << deter << std::endl;
+    
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+
+    printf("\nesinv(%d,%d)=%g+%g*j",i+1,j+1,GSL_REAL(gsl_matrix_complex_get(Sinv,i,
+    j)),GSL_IMAG(gsl_matrix_complex_get(Sinv,i,j)));
+        }
+    }
+    printf("\n");
+
+  delete[] test;
+  delete[] evals;
+  delete[] evecs;
 
   return 0;
 }
