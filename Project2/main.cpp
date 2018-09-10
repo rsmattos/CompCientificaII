c37ddbcd89ce5f1466407bb589194de1095c8aef
RafaelS <rafaelcpii93@gmail.com>
2018-09-10 08:05:07 -0300

    Moved execution from main to planetary system.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -23,10 +23,9 @@ struct ODE_set{
 #include "bodies.h"
 #include "../exercice2/ODE_solver.h"
 #include "function.h"
-// #include "planetary_system.h"
+#include "planetary_system.h"
 
 int main(){
-    int i;
     // setting planetay params
     struct Params p;
 
@@ -53,68 +52,10 @@ int main(){
     set.initial = 0.;
     set.final = 20.;
     set.step = 2.;
-    set.dimension = 4;
+    set.dimension = 2;
     set.function = &planetary2D;
 
-    // set up solver
-    ODE_solver solver(set.dimension);
-    solver.set_time(set);
-    solver.set_system(set.function, &p);
-    solver.set_kutta();
+    planetary_system(set, &p);
 
-    double *u = new double[set.dimension];
-
-    // calculate new positions and velocities for each planet
-    // transform position and velocity in variable vector
-    for(p.i = 0; p.i < p.planets; p.i++){
-        u[0] = p.planet[p.i].get_xx();
-        u[1] = p.planet[p.i].get_xy();
-        u[2] = p.planet[p.i].get_vx();
-        u[3] = p.planet[p.i].get_vy();
-
-        solver.initial_values(u);
-        solver.RK4_step();
-        solver.get_variables(u);
-
-        p.planet[p.i].new_position(u[0], u[1]);
-        p.planet[p.i].new_velocity(u[2], u[3]);
-    }
-
-    std::cout << "primeiro passo" << std::endl;
-
-    for(i = 0; i < p.planets; i++){
-        p.planet[i].update_positions();
-        p.planet[i].update_velocities();
-    
-        std::cout << "planet" << i << " (" << p.planet[i].get_xx() << ", " << p.planet[i].get_xy() << ")" << std::endl;
-    }
-
-    // calculate new positions and velocities for each planet
-    // transform position and velocity in variable vector
-    for(p.i = 0; p.i < p.planets; p.i++){
-        u[0] = p.planet[p.i].get_xx();
-        u[1] = p.planet[p.i].get_xy();
-        u[2] = p.planet[p.i].get_vx();
-        u[3] = p.planet[p.i].get_vy();
-
-        solver.initial_values(u);
-        solver.forward_euler_step();
-        solver.get_variables(u);
-
-        p.planet[p.i].new_position(u[0], u[1]);
-        p.planet[p.i].new_velocity(u[2], u[3]);
-    }
-
-    std::cout << "segundo passo" << std::endl;
-    
-    for(i = 0; i < p.planets; i++){
-        p.planet[i].update_positions();
-        p.planet[i].update_velocities();
-    
-        std::cout << "planet" << i << " (" << p.planet[i].get_xx() << ", " << p.planet[i].get_xy() << ")" << std::endl;
-    }
-
-    // update positions and velocities
     delete[] p.planet;
-    delete[] u;
 }
---
+++ Project2/planetary_system.h
@@ -0,0 +1,65 @@
+void planetary_system(struct ODE_set set, struct Params *p){
+    int i;
+
+    // set up solver
+    ODE_solver solver(set.dimension);
+    solver.set_time(set);
+    solver.set_system(set.function, p);
+    solver.set_kutta();
+
+    double *u = new double[2*set.dimension];
+
+    // calculate new positions and velocities for each planet
+    // transform position and velocity in variable vector
+    for(p->i = 0; p->i < p->planets; p->i++){
+        u[0] = p->planet[p->i].get_xx();
+        u[1] = p->planet[p->i].get_xy();
+        u[2] = p->planet[p->i].get_vx();
+        u[3] = p->planet[p->i].get_vy();
+
+        solver.initial_values(u);
+        solver.RK4_step();
+        solver.get_variables(u);
+
+        p->planet[p->i].new_position(u[0], u[1]);
+        p->planet[p->i].new_velocity(u[2], u[3]);
+    }
+
+    std::cout << "primeiro passo" << std::endl;
+
+    for(i = 0; i < p->planets; i++){
+        p->planet[i].update_positions();
+        p->planet[i].update_velocities();
+    
+        std::cout << "planet" << i << " (" << p->planet[i].get_xx() << ", " << p->planet[i].get_xy() << ")" << std::endl;
+    }
+
+    // calculate new positions and velocities for each planet
+    // transform position and velocity in variable vector
+    for(p->i = 0; p->i < p->planets; p->i++){
+        u[0] = p->planet[p->i].get_xx();
+        u[1] = p->planet[p->i].get_xy();
+        u[2] = p->planet[p->i].get_vx();
+        u[3] = p->planet[p->i].get_vy();
+
+        solver.initial_values(u);
+        solver.RK4_step();
+        solver.get_variables(u);
+
+        p->planet[p->i].new_position(u[0], u[1]);
+        p->planet[p->i].new_velocity(u[2], u[3]);
+    }
+
+    std::cout << "segundo passo" << std::endl;
+    
+    for(i = 0; i < p->planets; i++){
+        p->planet[i].update_positions();
+        p->planet[i].update_velocities();
+    
+        std::cout << "planet" << i << " (" << p->planet[i].get_xx() << ", " << p->planet[i].get_xy() << ")" << std::endl;
+    }
+
+    // update positions and velocities
+    delete[] u;
+
+}
