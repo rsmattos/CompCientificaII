8d4b4a7e5dda9304a2ee779978da432df3e88178
Rafael <rafaelcpii93@gmail.com>
2018-09-10 09:32:17 -0300

    Allocated right amount of memory to the solver

--- Project2/planetary_system.h
+++ Project2/planetary_system.h
@@ -2,7 +2,7 @@ void planetary_system(struct ODE_set set, struct Params *p){
     int i;
 
     // set up solver
-    ODE_solver solver(set.dimension);
+    ODE_solver solver(2*set.dimension);
     solver.set_time(set);
     solver.set_system(set.function, p);
     solver.set_kutta();
@@ -31,12 +31,12 @@ void planetary_system(struct ODE_set set, struct Params *p){
         p->planet[i].update_positions();
         p->planet[i].update_velocities();
     
-        std::cout << "planet" << i << " (" << p->planet[i].get_xx() << ", " << p->planet[i].get_xy() << ")" << std::endl;
+        std::cout << p->planet[i].get_name() << " (" << p->planet[i].get_xx() << ", " << p->planet[i].get_xy() << ")" << std::endl;
     }
 
     // calculate new positions and velocities for each planet
     // transform position and velocity in variable vector
-    for(p->i = 0; p->i < p->planets; p->i++){
+    for(p->i = 1; p->i < p->planets; p->i++){
         u[0] = p->planet[p->i].get_xx();
         u[1] = p->planet[p->i].get_xy();
         u[2] = p->planet[p->i].get_vx();
